c994adbfea6de9ba076effc6f54872980639a552
Kamal <kamaldeep734833@gmail.com>
2024-11-24 20:24:39 +0530

    Authcontroller Add

--- src/main/java/com/FoodHut/FoodHut/controller/AuthController.java
+++ src/main/java/com/FoodHut/FoodHut/controller/AuthController.java
@@ -1,15 +1,33 @@
 package com.FoodHut.FoodHut.controller;
 
-import org.springframework.stereotype.Controller;
+import com.FoodHut.FoodHut.config.JwtProvider;
+import com.FoodHut.FoodHut.repository.CartRepository;
+import com.FoodHut.FoodHut.repository.UserRepository;
+import com.FoodHut.FoodHut.service.CustomerUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Controller
-public class UserController {
+@RequestMapping("/auth")
+public class AuthController {
 
-    @GetMapping("/")
-    public String geeting(){
-        return "Every things fine";
-    }
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtProvider jwtProvider;
+
+    @Autowired
+    private CustomerUserDetailsService customerUserDetailsService;
+
+    @Autowired
+    private CartRepository cartRepository;
+
+
 }
