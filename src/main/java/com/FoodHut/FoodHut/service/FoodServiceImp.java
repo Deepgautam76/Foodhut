e7ca488fd60cd555d50c745fb21e71d0ddb3cd48
Kamal <kamaldeep734833@gmail.com>
2024-12-05 07:24:53 +0530

    implements foodDelete and foodFilter method

--- src/main/java/com/FoodHut/FoodHut/service/FoodServiceImp.java
+++ src/main/java/com/FoodHut/FoodHut/service/FoodServiceImp.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -38,24 +39,76 @@ public class FoodServiceImp implements FoodService {
 
     @Override
     public void deleteFood(Long foodId) throws Exception {
+        Food food=findFoodById(foodId);
+        food.setRestaurant(null);
+        foodRepository.save(food);
 
     }
 
     @Override
-    public List<Food> getRestaurantsFood(Long restaurantId, boolean isVegitarain, boolean isNonveg, boolean isSeasonal, String foodCategory) {
-        return null;
+    public List<Food> getRestaurantsFood(Long restaurantId,
+                                         boolean isVegitarain,
+                                         boolean isNonveg,
+                                         boolean isSeasonal,
+                                         String foodCategory) {
+
+        List<Food> foods=foodRepository.findByRestaurantId(restaurantId);
+
+        if(isVegitarain){
+            foods=filterByVegetarian(foods,isVegitarain);
+        }
+        if(isNonveg){
+            foods=filterByNonveg(foods,isNonveg);
+        }
+        if(isSeasonal){
+            foods=filterBySeasonal(foods, isSeasonal);
+        }
+        if(foodCategory!=null && !foodCategory.equals("")){
+            foods=filterByCategory(foods,foodCategory);
+        }
+
+        return foods;
     }
 
+    //Food filter by Category
+    private List<Food> filterByCategory(List<Food> foods, String foodCategory) {
+        return foods.stream().filter(food ->{
+            if(food.getFoodCategory()!=null){
+                return food.getFoodCategory().getName().equals(foodCategory);
+            }
+            return false;
+        }).collect(Collectors.toList());
+    }
+
+    //Food filter by Seasonal
+    private List<Food> filterBySeasonal(List<Food> foods, boolean isSeasonal) {
+        return foods.stream().filter(food -> food.isSeasonal()==isSeasonal).collect(Collectors.toList());
+    }
+
+    //Food filter by Non-veg
+    private List<Food> filterByNonveg(List<Food> foods, boolean isNonveg) {
+        return foods.stream().filter(food ->food.isVegetarian()==false).collect(Collectors.toList());
+    }
+
+    //Food filter by Vegetarian
+    private List<Food> filterByVegetarian(List<Food> foods, boolean isVegitarain) {
+        return foods.stream().filter(food -> food.isVegetarian()==isVegitarain).collect(Collectors.toList());
+    }
+
+
+    //This is a method for Search the food
     @Override
     public List<Food> searchFood(String keyword) {
         return null;
     }
 
+    //This is a method for search the food by id
     @Override
     public Food findFoodById(Long foodId) throws Exception {
         return null;
     }
 
+    //This method for the food stock available or not
     @Override
     public Food updateAvailibilityStatus(Long foodId) throws Exception {
         return null;
