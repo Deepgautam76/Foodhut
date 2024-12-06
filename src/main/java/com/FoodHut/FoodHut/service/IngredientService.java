d8e788acd937cb6e66d4f0e9fe5114beaf3508f7
Kamal <kamaldeep734833@gmail.com>
2024-12-06 19:13:58 +0530

    IngredientService all method declare

--- src/main/java/com/FoodHut/FoodHut/service/IngredientService.java
+++ src/main/java/com/FoodHut/FoodHut/service/IngredientService.java
@@ -1,10 +1,25 @@
 package com.FoodHut.FoodHut.service;
 
+import com.FoodHut.FoodHut.model.IngredientsItem;
+import com.FoodHut.FoodHut.model.IngredientsCategory;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface IngredientService {
 
+    IngredientsCategory createIngredientCategory(String name,Long restaurantId)throws Exception;
+
+    IngredientsCategory findIngredientCategoryById(Long id)throws Exception;
+
+    List<IngredientsCategory> findIngredientCategoryByRestaurantId(Long id) throws Exception;
+
+    IngredientsItem createIngredientItem(Long restaurantId, String ingredientName, Long categoryId)throws Exception;
+
+    List<IngredientsCategory> findRestaurantIngredients(Long restaurantId);
+
+    IngredientsItem updateStock(Long id)throws Exception;
 
 
 }
