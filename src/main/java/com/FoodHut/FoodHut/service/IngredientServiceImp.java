76a31425c89474927c9871a3bdbbc928fb9a8225
Kamal <kamaldeep734833@gmail.com>
2024-12-09 23:36:07 +0530

    implement all methods of IngredientService

--- src/main/java/com/FoodHut/FoodHut/service/IngredientServiceImp.java
+++ src/main/java/com/FoodHut/FoodHut/service/IngredientServiceImp.java
@@ -55,16 +55,36 @@ public class IngredientServiceImp implements IngredientService {
 
     @Override
     public IngredientsItem createIngredientItem(Long restaurantId, String ingredientName, Long categoryId) throws Exception {
-        return null;
+
+        Restaurant restaurant=restaurantService.findRestaurantById(restaurantId);
+        IngredientsCategory category=findIngredientCategoryById(categoryId);
+
+        IngredientsItem item=new IngredientsItem();
+        item.setName(ingredientName);
+        item.setRestaurant(restaurant);
+        item.setCategory(category);
+
+        IngredientsItem ingredient=ingredientsItemRepository.save(item);
+        category.getIngredientsItem().add(ingredient);
+        return ingredient;
     }
 
     @Override
-    public List<IngredientsCategory> findRestaurantIngredients(Long restaurantId) {
-        return null;
+    public List<IngredientsItem> findRestaurantIngredients(Long restaurantId) {
+        return ingredientsItemRepository.findByRestaurantId(restaurantId);
     }
 
     @Override
     public IngredientsItem updateStock(Long id) throws Exception {
-        return null;
+
+        Optional<IngredientsItem> optionalIngredientsItem=ingredientsItemRepository.findById(id);
+
+        if(optionalIngredientsItem.isEmpty()){
+            throw new Exception("ingredient not found");
+        }
+
+        IngredientsItem ingredientsItem=optionalIngredientsItem.get();
+        ingredientsItem.setInStoke(!ingredientsItem.isInStoke());
+        return ingredientsItemRepository.save(ingredientsItem);
     }
 }
