39e21b45a5f810f7d1a81b35d8cf569dae66d849
Kamal <kamaldeep734833@gmail.com>
2024-12-09 23:09:46 +0530

    add method in repo

--- src/main/java/com/FoodHut/FoodHut/repository/IngredientCategoryRepository.java
+++ src/main/java/com/FoodHut/FoodHut/repository/IngredientCategoryRepository.java
@@ -4,6 +4,10 @@ import com.FoodHut.FoodHut.model.IngredientsCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IngredientCategoryRepository extends JpaRepository<IngredientsCategory,Long> {
+
+    List<IngredientsCategory> findByRestaurantId(Long id);
 }
