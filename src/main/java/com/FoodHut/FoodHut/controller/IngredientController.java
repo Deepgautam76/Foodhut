f9460200377dbac6399bdce25765d066358b5572
Kamal <kamaldeep734833@gmail.com>
2024-12-11 00:13:15 +0530

    add new API end-point

--- src/main/java/com/FoodHut/FoodHut/controller/IngredientController.java
+++ src/main/java/com/FoodHut/FoodHut/controller/IngredientController.java
@@ -1,7 +1,9 @@
 package com.FoodHut.FoodHut.controller;
 
 import com.FoodHut.FoodHut.model.IngredientsCategory;
+import com.FoodHut.FoodHut.model.IngredientsItem;
 import com.FoodHut.FoodHut.request.IngredientCategoryRequest;
+import com.FoodHut.FoodHut.request.IngredientRequest;
 import com.FoodHut.FoodHut.service.IngredientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,9 +26,19 @@ public class IngredientController {
             @RequestBody IngredientCategoryRequest req
             ) throws Exception {
 
-        IngredientsCategory item=ingredientService.createIngredientCategory(req.getName(),req.getRestaurentId());
+        IngredientsCategory item=ingredientService.createIngredientCategory(req.getName(),req.getRestaurantId());
         return new ResponseEntity<>(item, HttpStatus.CREATED);
     }
+
+    //API end-point for creation of Ingredient
+    @PostMapping()
+    public ResponseEntity<?> createIngredient(
+            @RequestBody IngredientRequest req
+    ) throws Exception {
+        IngredientsItem item=ingredientService.createIngredientItem(req.getRestaurantId(),req.getName(),req.getCategoryId());
+        return new ResponseEntity<>(item,HttpStatus.CREATED);
+    }
+
 
 
 }
