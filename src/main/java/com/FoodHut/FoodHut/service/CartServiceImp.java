174153d19492e751772c7ed6713318a8713ad1a6
Kamal <kamaldeep734833@gmail.com>
2024-12-13 23:18:13 +0530

    implement all methods of cartService

--- src/main/java/com/FoodHut/FoodHut/service/CartServiceImp.java
+++ src/main/java/com/FoodHut/FoodHut/service/CartServiceImp.java
@@ -1,8 +1,86 @@
 package com.FoodHut.FoodHut.service;
 
+import com.FoodHut.FoodHut.model.Cart;
+import com.FoodHut.FoodHut.model.CartItem;
+import com.FoodHut.FoodHut.model.Food;
+import com.FoodHut.FoodHut.model.User;
+import com.FoodHut.FoodHut.repository.CartItemRepository;
+import com.FoodHut.FoodHut.repository.CartRepository;
+import com.FoodHut.FoodHut.request.AddCartItemRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CartServiceImp {
+public class CartServiceImp implements CartService {
 
+    @Autowired
+    private CartRepository cartRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private CartItemRepository cartItemRepository;
+
+    @Autowired
+    private FoodService foodService;
+
+    @Override
+    public CartItem addItemToCart(AddCartItemRequest req, String jwt) throws Exception {
+
+        User user=userService.findUserByJwtToken(jwt);
+
+        Food food=foodService.findFoodById(req.getFoodId());
+
+        Cart cart=cartRepository.findByCustomerId(user.getId());
+
+        for (CartItem cartItem:cart.getItem()){
+            if(cartItem.getFood().equals(food)){
+                int newQuantity=cartItem.getQuantity()+req.getQuantity();
+                return updateCartItemQuantity(cartItem.getId(),newQuantity);
+            }
+        }
+        CartItem newCartItem=new CartItem();
+        newCartItem.setFood(food);
+        newCartItem.setCart(cart);
+        newCartItem.setQuantity(req.getQuantity());
+        newCartItem.setIngredients(req.getIngredients());
+        newCartItem.setTotalPrice(req.getQuantity()*food.getPrice());
+
+        CartItem savedCartItem=cartItemRepository.save(newCartItem);
+
+        cart.getItem().add(savedCartItem);
+
+        return savedCartItem;
+    }
+
+    @Override
+    public CartItem updateCartItemQuantity(Long cardItemId, int quantity) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Cart removeItemFromCart(Long cartItemId, String jwt) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Long calculateCartTotals(Cart cart) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Cart findCartById(Long id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Cart findCartByUserId(Long userId) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Cart clearCart(Long userId) throws Exception {
+        return null;
+    }
 }
