08190810bd0d9ed1e4bf5a561e305d963e691bac
Kamal <kamaldeep734833@gmail.com>
2024-12-14 20:47:00 +0530

    completed all methods of OrderServiceImp

--- src/main/java/com/FoodHut/FoodHut/service/OrderServiceImp.java
+++ src/main/java/com/FoodHut/FoodHut/service/OrderServiceImp.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class OrderServiceImp implements OrderService {
@@ -59,30 +61,78 @@ public class OrderServiceImp implements OrderService {
         List<OrderItem> orderItems=new ArrayList<>();
 
         for (CartItem cartItem:cart.getItems()){
+            OrderItem orderItem=new OrderItem();
+            orderItem.setFood(cartItem.getFood());
+            orderItem.setIngredients(cartItem.getIngredients());
+            orderItem.setQuantity(cartItem.getQuantity());
+            orderItem.setTotalPrice(cartItem.getTotalPrice());
+
+            OrderItem savedOrderItem=orderItemRepository.save(orderItem);
+            orderItems.add(savedOrderItem);
 
 
         }
 
-        return null;
+        Long totalPrice=cartService.calculateCartTotals(cart);
+
+        createOrder.setItems(orderItems);
+        createOrder.setTotalPrice(totalPrice);
+
+        Order savedOrder=orderRepository.save(createOrder);
+        restaurant.getOrders().add(savedOrder);
+
+
+        return createOrder;
     }
 
     @Override
     public Order updateOrder(Long orderId, String orderStatus) throws Exception {
-        return null;
+        Order order=findOrderById(orderId);
+        if(orderStatus.equals("OUT_FOR_DELIVERY")
+                || orderStatus.equals("DELIVERED")
+                || orderStatus.equals("COMPLETED")
+                || orderStatus.equals("PENDING")
+        ){
+            order.setOrderStatus(orderStatus);
+            return orderRepository.save(order);
+
+        }
+        throw new  Exception("Please select a valid order status");
     }
 
     @Override
     public void cancelOrder(Long orderId) throws Exception {
+        Order order=findOrderById(orderId);
+        orderRepository.deleteById(orderId);
 
     }
 
     @Override
     public List<Order> getUserOrder(Long userId) throws Exception {
-        return null;
+
+        return orderRepository.findByCustomerId(userId);
     }
 
     @Override
     public List<Order> getRestaurantOrder(Long restaurantId, String orderStatus) throws Exception {
-        return null;
+        List<Order> orders=orderRepository.findByRestaurantId(restaurantId);
+
+        if(orderStatus!=null){
+            orders = orders.stream().filter(order ->
+                    order.getOrderStatus().equals(orderStatus)).collect(Collectors.toList());
+        }
+
+        return orders;
+    }
+
+    @Override
+    public Order findOrderById(Long orderId) throws Exception {
+
+        Optional<Order> optionalOrder=orderRepository.findById(orderId);
+
+        if(optionalOrder.isEmpty()){
+            throw  new Exception("order not found");
+        }
+        return optionalOrder.get();
     }
 }
