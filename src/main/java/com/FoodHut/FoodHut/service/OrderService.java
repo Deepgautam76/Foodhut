fc84677898147b29b6fc8d9861ad76ccc89efcaa
Kamal <kamaldeep734833@gmail.com>
2024-12-14 14:27:47 +0530

    create OrderService interface

---
+++ src/main/java/com/FoodHut/FoodHut/service/OrderService.java
@@ -0,0 +1,24 @@
+package com.FoodHut.FoodHut.service;
+
+import com.FoodHut.FoodHut.model.Order;
+import com.FoodHut.FoodHut.model.User;
+import com.FoodHut.FoodHut.request.OrderRequest;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderService {
+
+    Order createOrder(OrderRequest order, User user) throws Exception;
+
+    Order updateOrder(Long orderId,String orderStatus) throws Exception;
+
+    void cancelOrder(Long orderId) throws Exception;
+
+    List<Order> getUserOrder(Long userId)throws Exception;
+
+    List<Order> getRestaurantOrder(Long restaurantId,String orderStatus)throws Exception;
+
+
+}
