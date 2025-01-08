90a7780c1d35fd8d59ebcd959b7d97ceeecf0f57
Kamal <kamaldeep734833@gmail.com>
2025-01-08 23:58:26 +0530

    add in swaggerConfig file JWT access

--- src/main/java/com/FoodHut/FoodHut/config/SwaggerConfig.java
+++ src/main/java/com/FoodHut/FoodHut/config/SwaggerConfig.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 /**
  * This is the swagger config
  * For generating the documentation
@@ -18,7 +20,13 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("My Food hut projects APIs")
                         .version("v1.0")
-                        .description("project API end-point documents")
+                        .description("project API end-point documents"))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("Bearer Authentication", new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("Bearer")
+                                        .bearerFormat("JWT"))
                 );
     }
 
